8df9665fe8c6a70c088c65f011c53b47b3cd3f8c
Adil Khan <akhan@kpd.be>
2020-02-27 14:25:39 +0100

    remove old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ShoplistComponent } from './components/shop/shoplist/shoplist.component';
-import { ShoplistItemComponent } from './components/shop/shoplist-item/shoplist-item.component';
 import { InventoryListComponent } from './components/inventory/inventory-list/inventory-list.component';
 import { InventoryItemComponent } from './components/inventory/inventory-item/inventory-item.component';
 import { ShopItemComponent } from './components/shop/shop-item/shop-item.component';
@@ -17,20 +15,15 @@ import { CartItemComponent } from './components/cart/cart-item/cart-item.compone
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoplistComponent,
-    ShoplistItemComponent,
     InventoryListComponent,
     InventoryItemComponent,
     ShopItemComponent,
     ShopListComponent,
     CartListComponent,
-    CartItemComponent
+    CartItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
