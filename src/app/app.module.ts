fe1f65b2e28b5e641a9aea09be08c63bb6d910b3
Adil Khan <akhan@kpd.be>
2020-02-27 14:23:30 +0100

    Added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ShoplistComponent } from './components/shop/shoplist/shoplist.component';
+import { ShoplistItemComponent } from './components/shop/shoplist-item/shoplist-item.component';
+import { InventoryListComponent } from './components/inventory/inventory-list/inventory-list.component';
+import { InventoryItemComponent } from './components/inventory/inventory-item/inventory-item.component';
+import { ShopItemComponent } from './components/shop/shop-item/shop-item.component';
+import { ShopListComponent } from './components/shop/shop-list/shop-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoplistComponent,
+    ShoplistItemComponent,
+    InventoryListComponent,
+    InventoryItemComponent,
+    ShopItemComponent,
+    ShopListComponent
   ],
   imports: [
     BrowserModule,
