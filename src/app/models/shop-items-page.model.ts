1d6c75cbdc70132ab99b8e62e644647a4a48920a
Adil Khan <akhan@kpd.be>
2020-03-09 11:13:55 +0100

    add items page model

---
+++ src/app/models/shop-items-page.model.ts
@@ -0,0 +1,27 @@
+export class ShopItemsPage {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: [
+    {
+      name: string;
+      url: string;
+    },
+  ];
+
+  constructor(dto?: IShopItemsPageDTO) {
+    Object.assign(this, dto);
+  }
+}
+
+export interface IShopItemsPageDTO {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: [
+    {
+      name: string;
+      url: string;
+    },
+  ];
+}
